ec06ac9ef8ec55849f107713b5351192a1abbf18
Jakub Andrzejewski <jakubandrzejewski@op.pl>
2023-11-15 17:00:27 +0100

    fix

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,10 @@
-import Koa from "koa";
-import cors from "@koa/cors";
-import { bodyParser } from "@koa/bodyparser";
-import { healthRouter } from "./context/health.context";
-import { internalLocalStorage } from "./config/internal.local.storage.config";
-import { errorHandler } from "./errors/error.handler";
-import { productRouter } from "./context/product.context";
+import Koa from 'koa';
+import cors from '@koa/cors';
+import { bodyParser } from '@koa/bodyparser';
+import { healthRouter } from './context/health.context';
+import { internalLocalStorage } from './config/internal.local.storage.config';
+import { errorHandler } from './errors/error.handler';
+import { productRouter } from './context/product.context';
 
 export const app = new Koa()
   .use(errorHandler)
@@ -12,4 +12,6 @@ export const app = new Koa()
   .use(bodyParser())
   .use(healthRouter.getRoutes())
   .use(internalLocalStorage.startStorage)
+  .use(internalLocalStorage.storeRequestId)
+  .use(internalLocalStorage.storeUserId)
   .use(productRouter.getRoutes());
