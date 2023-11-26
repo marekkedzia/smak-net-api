4aec4c30226fcd018e655d532611367815596ecf
Jakub Andrzejewski <jakubandrzejewski@op.pl>
2023-11-26 14:31:08 +0100

    Added category for products

--- src/modules/product/product.validator.ts
+++ src/modules/product/product.validator.ts
@@ -1,9 +1,12 @@
 import { z as zod } from "zod";
 import { variablesConfig } from "../../config/variables.config";
+import { CATEGORY } from './product.interfaces';
 
+const categoryValues = Object.values(CATEGORY) as [string, ...string[]];
 
 export const productValidator = zod.object({
     name: zod.string().max(variablesConfig.maxProductNameLength).nonempty(),
+    category: zod.enum(categoryValues),
     description: zod.string().max(variablesConfig.maxProductDescriptionLength).nonempty(),
     price: zod.number().positive()
   }
